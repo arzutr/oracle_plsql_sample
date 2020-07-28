e080c1aaccded95279a039660647795e8bd1108a
arzutr <52047628+arzutr@users.noreply.github.com>
2020-07-28 22:05:15 +0300

    Create setup_muplsql.sql

---
+++ School/test/setup_muplsql.sql
@@ -0,0 +1,12 @@
+
+grant all on mutation.tmutation to school;
+
+grant all on  mutation.tmutation_testcase to school;
+
+grant all  on mutation.tmutation_testcase_exec to school;
+
+grant all on  mutation.tmutation_operations to school;
+
+grant all on  mutation.tmutation_operators to school;
+
+
