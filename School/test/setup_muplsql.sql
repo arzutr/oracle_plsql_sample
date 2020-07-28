28e6e0187e67a3db83dcf2467e6c88953cf54862
arzutr <52047628+arzutr@users.noreply.github.com>
2020-07-28 22:11:32 +0300

    Update setup_muplsql.sql

--- School/test/setup_muplsql.sql
+++ School/test/setup_muplsql.sql
@@ -10,3 +10,17 @@ grant all on  mutation.tmutation_operations to school;
 grant all on  mutation.tmutation_operators to school;
 
 
+
+
+create public synonym  tmutation  for  mutation.tmutation; 
+
+
+create public synonym  tmutation_testcase  for  mutation.tmutation_testcase; 
+
+create public synonym  tmutation_testcase_exec  for  mutation.tmutation_testcase_exec; 
+
+create public synonym  tmutation_operations  for  mutation.tmutation_operations; 
+
+
+create public synonym  tmutation_operators  for  mutation.tmutation_operators; 
+
